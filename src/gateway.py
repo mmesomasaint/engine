a73eb8db10819073fe82984c83dd13cd176391fc
Webmonk <104903171+mmesomasaint@users.noreply.github.com>
2026-03-28 20:54:51 +0000

    Fix: allowed origins

--- src/gateway.py
+++ src/gateway.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://optima-portals.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://optima-portals-six.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
